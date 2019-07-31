8dfb9ada083ea4df4d6fab900cab5c4364e5e279
moro <mimijjag99@gmali.com>
2019-07-31 17:16:23 +0900

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 
 def home(request):
     blogs = Blog.objects #blog 안의 데이터를 나타내는 변수
-    return render(request, 'home.html', {'blogs':blogs})
+    blog_list = Blog.objects.all() #블로그 모든 글 대상
+    paginator = Paginator(blog_list,3) #3씩 한페이지
+    page = request.GET.get('page') #request된 페이지를 알아네고 변수에 담음
+    posts = paginator.get_page(page)
+
+    return render(request, 'home.html', {'blogs':blogs, 'posts':posts})
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
